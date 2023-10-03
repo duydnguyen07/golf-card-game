2ee28e36de19b7b00f6f564fa0fdbc94d64b9839
Duy Nguyen <blinkblinklink@aim.com>
2023-10-03 23:40:59 +0200

    added backend

---
+++ apps/gold-card-game-server/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'golf-card-game-server',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/gold-card-game-server',
+};
